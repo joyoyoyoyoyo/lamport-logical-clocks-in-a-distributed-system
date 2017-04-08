af3447aca9f9c849a9121374856a9e4187d14d02
Angel Ortega <an22ortega@gmail.com>
2017-04-07 20:29:25 -0700

    sample resources added

---
+++ src/main/scala/App.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by bahamut on 4/7/17.
+  */
+
+// program should take the port number to bind a socket (first argument)
+// the input file path as its arguments (second argument) (e.g. /asg1 5001 p1.txt)
+
+
+val num_processes = 3 // NUM_PROCESSES
+
+val processes = new Array[String](num_processes)
+// each process has two threads
