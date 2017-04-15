732913a49f34f88a3c1d48069a12cd11a9d9d749
Angel Ortega <an22ortega@gmail.com>
2017-04-15 15:30:11 -0700

    test execution sample

--- src/test/scala/lamport/MainTest.scala
+++ src/test/scala/lamport/MainTest.scala
@@ -1,5 +1,23 @@
 package lamport
 
-class MainTest {
+import java.util
 
+import org.scalatest._
+
+class MainTest extends FlatSpec{
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new util.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new util.Stack[String]
+    assertThrows[NoSuchElementException] {
+      emptyStack.pop()
+    }
+  }
 }
